84795023430336cbc00ed248f48d2192f0508454
Aaron Trovillion <aaron.trovillion@gmail.com>
2018-08-31 15:12:13 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDIAxD7B0TpsiTGxmWjyzrx_OotCDZtSTA",
+  authDomain: "bloc-react-chat.firebaseapp.com",
+  databaseURL: "https://bloc-react-chat.firebaseio.com",
+  projectId: "bloc-react-chat",
+  storageBucket: "bloc-react-chat.appspot.com",
+  messagingSenderId: "1005103712063"
+};
+firebase.initializeApp(config);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {rooms: RoomList}
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div className="room-list">
+          <RoomList
+            firebase={firebase}
+           />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
