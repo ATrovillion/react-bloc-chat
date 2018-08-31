79a9d1b3c81f13619e5122af1dd7f242ff40a3b8
Aaron Trovillion <aaron.trovillion@gmail.com>
2018-08-31 16:00:28 -0400

    add room-list component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDIAxD7B0TpsiTGxmWjyzrx_OotCDZtSTA",
+  authDomain: "bloc-react-chat.firebaseapp.com",
+  databaseURL: "https://bloc-react-chat.firebaseio.com",
+  projectId: "bloc-react-chat",
+  storageBucket: "bloc-react-chat.appspot.com",
+  messagingSenderId: "1005103712063"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {rooms: RoomList}
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="room-list">
+          <RoomList
+            firebase={firebase}
+           />
+        </div>
       </div>
     );
   }
